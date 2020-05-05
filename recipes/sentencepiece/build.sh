d9f0a6bb5191454aa07c7ea89120f93fa1c8d9fa
rluria14 <ryanlur14@gmail.com>
2020-05-05 15:17:16 -0400

    nproc alias on mac

--- recipes/sentencepiece/build.sh
+++ recipes/sentencepiece/build.sh
@@ -2,14 +2,18 @@
 
 export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}
 export LD_LIBRARY_PATH=${PREFIX}/lib:${LD_LIBRARY_PATH}
-export INCLUDE=${PREFIX}/include
 
 mkdir build
 cd build
 
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib -DCMAKE_INSTALL_INCLUDEDIR=${PREFIX}/include -DCMAKE_AR="${AR}" -DSPM_ENABLE_TCMALLOC=OFF -S ..
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib -DCMAKE_AR="${AR}" -DSPM_ENABLE_TCMALLOC=OFF -S ..
 
-make -j $(nproc)
+if [[ "$target_platform" == linux* ]]; then
+  make -j $(nproc)
+elif [[ $target_platform == "osx-64" ]]; then
+  alias nproc="sysctl -n hw.logicalcpu"
+  make -j $(nproc)
+fi
 
 make install
 
