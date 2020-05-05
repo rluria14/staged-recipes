e8988b4b055bbfd96fc46a6a99813d10a2903c94
rluria14 <ryanlur14@gmail.com>
2020-05-05 12:25:20 -0400

    recipe for sentencepiece

---
+++ recipes/sentencepiece/build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}
+export LD_LIBRARY_PATH=${PREFIX}/lib:${LD_LIBRARY_PATH}
+
+mkdir build
+cd build
+
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib -DCMAKE_AR="${AR}" -DSPM_ENABLE_TCMALLOC=OFF -S ..
+
+make -j $(nproc)
+
+make install
+
+if [[ "$target_platform" == linux* ]]; then
+  ldconfig -v -N
+elif [[ $target_platform == "osx-64" ]]; then
+  update_dyld_shared_cache
+fi
+
+cd ../python
+
+${PYTHON} -m pip install . -vv
