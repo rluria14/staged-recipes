46205cd1257e83c8eec516dcf9df666196d7d07e
rluria14 <ryanlur14@gmail.com>
2021-01-28 14:31:30 -0500

    bash changes

--- recipes/hootenanny/build.sh
+++ recipes/hootenanny/build.sh
@@ -3,10 +3,10 @@
 source ./SetupEnv.sh
 
 ### configure
-#aclocal && autoconf && autoheader && automake --add-missing && ./configure --with-services --with-uitests --with-rnd --with-coverage
+aclocal && autoconf && autoheader && automake --add-missing && ./configure --with-services --with-uitests --with-rnd --with-coverage
 
 ### build simple
-./configure && make -j$(nproc)
+./configure && make -sj$(nproc)
 
 ### build complete
 #./configure --with-rnd --with-services && make -j$(nproc)
