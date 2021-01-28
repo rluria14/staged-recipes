941e01626d5ad5faa1455a2a637f23cc1b6b8c63
rluria14 <ryanlur14@gmail.com>
2021-01-28 12:48:37 -0500

    cxx 11?

--- recipes/hootenanny/build.sh
+++ recipes/hootenanny/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export CXXFLAGS="${CXXFLAGS} -std=c++11"
+
 ### configure
 aclocal && autoconf && autoheader && automake && ./configure --with-services --with-uitests --with-rnd --with-coverage
 
