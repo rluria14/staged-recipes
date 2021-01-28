b61c145027f62249f4dc2da648d8369e6844bd81
rluria14 <ryanlur14@gmail.com>
2021-01-28 15:18:33 -0500

    remove hoothome

--- recipes/hootenanny/build.sh
+++ recipes/hootenanny/build.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 
-### set HOOT_HOME
-if [ -z "$HOOT_HOME" ]; then
-    HOOT_HOME=~/hoot
-fi
-echo HOOT_HOME: $HOOT_HOME
+#### set HOOT_HOME
+#if [ -z "$HOOT_HOME" ]; then
+#    HOOT_HOME=~/hoot
+#fi
+#echo HOOT_HOME: $HOOT_HOME
+#
+#### Temp change until we get the C++11 support into develop
+#cd $HOOT_HOME
 
-### Temp change until we get the C++11 support into develop
-cd $HOOT_HOME
-cp LocalConfig.pri.orig LocalConfig.pri
-echo "QMAKE_CXXFLAGS += -std=c++11" >> LocalConfig.pri
 
 echo "SetupEnv.sh"
 source ./SetupEnv.sh
 
-echo "Building Hoot"
 # Going to remove this so that it gets updated
 if [ -f missing ]; then
   rm -f missing
@@ -25,18 +23,25 @@ aclocal && autoconf && autoheader && automake --add-missing --copy
 
 ./configure \
   --with-rnd \
-  --without-services \
-  --without-postgresql \
-  --prefix=/usr/ \
-  --datarootdir=/usr/share/hootenanny/ \
-  --docdir=/usr/share/doc/hootenanny/ \
-  --localstatedir=/var/lib/hootenanny/ \
-  --libdir=/usr/lib64 \
-  --sysconfdir=/etc/
+  --with-services \
+  --with-coverage \
+  --with-uitetests
 
-### build simple
+if [ ! -f LocalConfig.pri ] && ! grep --quiet QMAKE_CXX LocalConfig.pri; then
+    echo 'Customizing LocalConfig.pri...'
+    cp LocalConfig.pri.orig LocalConfig.pri
+    sed -i s/"QMAKE_CXX=g++"/"#QMAKE_CXX=g++"/g LocalConfig.pri
+    sed -i s/"#QMAKE_CXX=ccache g++"/"QMAKE_CXX=ccache g++"/g LocalConfig.pri
+    if [ "$BUILD_DEBUG" == "yes" ]; then
+        echo 'Building in DEBUG mode...'
+        sed -i s/"CONFIG += release"/"#CONFIG += release"/ LocalConfig.pri
+        sed -i s/"CONFIG -= debug"/"#CONFIG -= debug"/ LocalConfig.pri
+        sed -i s/"#CONFIG += debug"/"CONFIG += debug"/ LocalConfig.pri
+        sed -i s/"#CONFIG -= release"/"CONFIG -= release"/ LocalConfig.pri
+    fi
+fi
+
+echo "Building Hoot"
+### build
 make -s clean && make -sj$(nproc)
-
-# This stops the install dieing if it cant copy PDF files.
-touch docs/empty.pdf
 
